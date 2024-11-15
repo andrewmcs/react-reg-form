116055fba1d4ef367d2f1cff9c3ad95a18c63fab
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2024-11-15 17:40:03 +0000

    started reg form

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>First Name <sup>*</sup></label>
+            <input placeholder="First Name"></input>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
